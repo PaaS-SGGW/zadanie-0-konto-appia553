37c4553aebd66bb9044958176879487662d9559d
appia553 <anna.piasecka1@vp.pl>
2018-12-01 16:06:46 +0100

    Fixes

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
 
-echo "Error 404, no pancakes found"
+include ('db.php')
+
+echo "Error 404, no pancakes found<br>"
+
+ $sql = 'SELECT * FROM users';
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $rowCount = $stmt->rowCount();
+  $details = $stmt->fetch();
+  print_r ($details);
 
 ?>
