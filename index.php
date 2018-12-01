d71378721358c4877de2cc0cebdec9e9fbcbe65d
appia553 <anna.piasecka1@vp.pl>
2018-12-01 16:18:09 +0100

    More fixes

--- index.php
+++ index.php
@@ -9,9 +9,6 @@ echo "Error 404, no pancakes found<br>";
   $stmt->execute();
   $rowCount = $stmt->rowCount();
   $details = $stmt->fetch();
-  echo "Users: <br>";
-  foreach($details as $i) {
-	  echo $i['name'] . " ";
-	  echo $i['email'] . " <br>";
+  echo $details";
   }
 ?>
