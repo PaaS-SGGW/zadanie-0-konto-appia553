f3384aa930f0204ff0de3261205c59686f9a5321
appia553 <anna.piasecka1@vp.pl>
2018-12-01 16:16:22 +0100

    Added better layout

--- index.php
+++ index.php
@@ -9,6 +9,9 @@ echo "Error 404, no pancakes found<br>";
   $stmt->execute();
   $rowCount = $stmt->rowCount();
   $details = $stmt->fetch();
-  print_r ($details);
-
+  echo "Users: <br>";
+  foreach($details as $i) {
+	  echo $i['name'] . " ";
+	  echo $i['email'] . " <br>";
+  }
 ?>
